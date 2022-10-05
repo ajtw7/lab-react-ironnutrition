e4f0b548bfeb0a5ca9de498834e0b686b8cb556d
Andrew <ayjtw7@gmail.com>
2022-10-05 01:20:34 -0400

    initial commit; iterations 1, 2, 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import foods from '../src/foods.json';
+import FoodBox from '../src/components/FoodBox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <h1>Food List</h1>
+      { foods.map(food => {
+       return( 
+          <FoodBox key={food.name} food={food} />
+      );  
+    })
+
+  } 
+  </div>
+)}
 
 export default App;
